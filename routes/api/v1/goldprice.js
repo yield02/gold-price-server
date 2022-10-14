cfa652b0d601c7f6ff5e0d23f7e014c6f4a31e12
Yield <83912710+yield02@users.noreply.github.com>
2022-10-14 22:25:42 +0700

    FIX SELECT

--- routes/api/v1/goldprice.js
+++ routes/api/v1/goldprice.js
@@ -3,7 +3,7 @@ const conn = require("../../../db");
 
 router.get("/", (req, res, next) => {
   conn.query(
-    "SELECT * FROM GIAVANG, VANG, TINH_TP WHERE GIAVANG.MAVANG = VANG.MAVANG AND GIAVANG.MATTP = TINH_TP.MATTP",
+    "SELECT * FROM GIAVANG, LOAIVANG, KHUVUC WHERE GIAVANG.MALV = LOAIVANG.MALV AND GIAVANG.MAKV = KHUVUC.MAKV",
     (err, result) => {
       if (err) throw err;
       res.send(result);
@@ -13,7 +13,7 @@ router.get("/", (req, res, next) => {
 
 router.get("/:maVang", (req, res, next) => {
   conn.query(
-    `SELECT * FROM GIAVANG, VANG, TINH_TP WHERE GIAVANG.MAVANG = VANG.MAVANG AND GIAVANG.MATTP = TINH_TP.MATTP AND VANG.MAVANG='${req.params.maVang}'`,
+    `SELECT * FROM GIAVANG, LOAIVANG, KHUVUC WHERE GIAVANG.MALV = LOAIVANG.MALV AND GIAVANG.MAKV = KHUVUC.MAKV AND LOAIVANG.MALV='${req.params.maVang}'`,
     (err, result) => {
       if (err) throw err;
       res.send(result);
@@ -23,14 +23,14 @@ router.get("/:maVang", (req, res, next) => {
 
 router.post("/them-vang", (req, res, next) => {
   conn.query(
-    `INSERT INTO VANG (MAVANG, TENVANG) VALUES ('${req.query.mavang}', '${req.query.tenvang}')`
+    `INSERT INTO LOAIVANG (MALV, TENLV) VALUES ('${req.query.mavang}', '${req.query.tenvang}')`
   );
   res.send(req.query);
 });
 
 router.post("/them-ttp", (req, res, next) => {
   conn.query(
-    `INSERT INTO TINH-TP (MATTP, TENTTP) VALUES ('${req.query.mattp}', '${req.query.tenttp}')`
+    `INSERT INTO KHUVUC (MAKV, TENKV) VALUES ('${req.query.mattp}', '${req.query.tenttp}')`
   );
   res.send(req.query);
 });
